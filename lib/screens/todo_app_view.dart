e784e6399b5380012b57f7cbef8248e64f82a314
Korugan32 <tahacansenel@gmail.com>
2025-06-03 22:39:15 +0300

    first commit

---
+++ lib/screens/todo_app_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TodoAppView extends StatefulWidget {
+  const TodoAppView({super.key});
+
+  @override
+  State<TodoAppView> createState() => _TodoAppViewState();
+}
+
+class _TodoAppViewState extends State<TodoAppView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
